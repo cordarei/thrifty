cb4444450906dfc611886de28261e30755992f86
JOSEPH HOWARD IRWIN <joseph-i@leopard175.naist.jp>
2011-01-25 17:49:15 +0900

    First commit for project Thrifty!
      - Edited Gemfile & .gitignore
      - config/environments/development.rb had blank lines?
      - deleted default index.html and rails.png

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thrifty::Application.config.secret_token = '07efd3ebbe37f8a3585ffab6e8bd88cfae8b7eda8203b6a21c946f76384d37872a282bfc6f99ca014d33e0ecf954a340a56274e8624cbd52fd8ed193a1d43490'
