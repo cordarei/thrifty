ef2c7d8b7800c3499e27b61d8917465047b1f14f
JOSEPH HOWARD IRWIN <joseph-i@leopard175.naist.jp>
2011-01-26 10:03:59 +0900

    modified index page with sample list

--- spec/controllers/purchases_controller_spec.rb
+++ spec/controllers/purchases_controller_spec.rb
@@ -1,11 +1,22 @@
 require 'spec_helper'
 
 describe PurchasesController do
+  render_views
 
   describe "GET 'index'" do
     it "should be successful" do
-      get 'index'
+      get :index
       response.should be_success
+    end
+
+    it "should have appropriate heading" do
+      get :index
+      response.should have_selector("h1", :content => "Purchases")
+    end
+
+    it "should have list of purchases" do
+      get :index
+      response.should have_selector("div.purchases")
     end
   end
 
